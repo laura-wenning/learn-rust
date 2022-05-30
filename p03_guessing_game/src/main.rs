b9f9663ce8fa004b7bc8c03e44aa81131f58d3fd
Laura Wenning <laura.r.wenning@gmail.com>
2022-05-30 17:40:13 +0000

    Finishes the guessing game. Learned about rand, making functions, loops, and error handling

--- p03_guessing_game/src/main.rs
+++ p03_guessing_game/src/main.rs
@@ -1,13 +1,43 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
   println!("Guess the number!");
-  println!("Please input your guess.");
   
-  let mut guess = String::new();
+  let actual = rand::thread_rng().gen_range(1..101);
+  
+  loop {
+    let guess = match get_guess() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+
+    match guess.cmp(&actual) {
+      Ordering::Less => println!("Too small"),
+      Ordering::Equal => {
+        println!("You got it!");
+        break;
+      },
+      Ordering::Greater => println!("Too big"),
+    }
+  }
+}
+
+
+fn get_guess() -> Result<u32, std::num::ParseIntError> {
+  println!("Please input your guess.");
+
+  let mut raw_guess = String::new();
   io::stdin()
-    .read_line(&mut guess)
+    .read_line(&mut raw_guess)
     .expect("Failed to read line");
 
+  let guess: u32 = match raw_guess.trim().parse() {
+    Ok(num) => num,
+    Err(e) => return Err(e),
+  };
   println!("You guessed: {}", guess);
+
+  return Ok(guess);
 }
